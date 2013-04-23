63ec5307d01a13f84685f840fa9998f2b4ed19cf
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2013-04-23 10:48:46 -0400

    Added NGUI. Working on File Menu

---
+++ Unity Research TherapistInt/Assets/Scripts/UIActionReciever.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Recieves actions created by NGUI
+/// </summary>
+
+public class UIActionReciever : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
