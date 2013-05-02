4d0826d98225dbc0274a4170186399cbf0f33e31
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2013-05-02 09:43:48 -0400

    play, pause, stop work correctly in interface. Now to connect!

--- Unity Research TherapistInt/Assets/Scripts/UIMenuActionReciever.cs
+++ Unity Research TherapistInt/Assets/Scripts/UIMenuActionReciever.cs
@@ -5,7 +5,31 @@ using System.Collections;
 /// Recieves actions created by NGUI
 /// </summary>
 
-public class UIMenuActionReciever : MonoBehaviour {
+public class UIMenuActionReciever : MonoBehaviour
+{
+
+    #region global variables
+
+    /// <summary>
+    /// enum which checks if play button is playing or is paused
+    /// </summary>
+    public enum ButtonState
+    {
+        Playing,
+        Paused,
+        Recording,
+        Not_Recording
+    };
+
+    /// <summary>
+    /// instance of enum which checks state of play button
+    /// </summary>
+    ButtonState playButtonStatus = ButtonState.Paused;
+
+    /// <summary>
+    /// Instance of enum which checks the state of the Record button
+    /// </summary>
+    ButtonState recordButtonStatus = ButtonState.Not_Recording;
 
     /// <summary>
     /// Currently selected file. Set by UILoadPanel
@@ -38,9 +62,26 @@ public class UIMenuActionReciever : MonoBehaviour {
     /// </summary>
     public GameObject ExportGroup;
 
+    /// <summary>
+    /// reference to play button
+    /// </summary>
+    public GameObject PlayButton;
+
+    /// <summary>
+    /// reference to record button
+    /// </summary>
+    public GameObject RecordButton;
+
+    /// <summary>
+    /// selected index
+    /// </summary>
     int currentIndex = 0;
 
-	/// <summary>
+    #endregion
+
+    #region main callback method
+
+    /// <summary>
 	/// Callback which is called every time a button in the UI
 	/// is selected	
 	/// </summary>
@@ -83,15 +124,35 @@ public class UIMenuActionReciever : MonoBehaviour {
                     break;
 
                 case "Open":
-                    openFile();
+                    openFileButtonPressed();
                     break;
 
                 case "Save":
-                    saveFile();
+                    saveFileButtonPressed();
                     break;
 
                 case "Export":
-                    exportFile();
+                    exportFileButtonPressed();
+                    break;
+
+                case "New":
+                    newButtonPressed();
+                    break;
+
+                case "Play":
+                    playPauseButtonPressed();
+                    break;
+
+                case "Stop":
+                    stopButtonPressed();
+                    break;
+
+                case "Record":
+                    recordButtonPressed();
+                    break;
+
+                case "Pause":
+                    playPauseButtonPressed();
                     break;
 
                 default:
@@ -103,6 +164,160 @@ public class UIMenuActionReciever : MonoBehaviour {
         }
 
 	}
+
+    #endregion
+
+    /// <summary>
+    /// changes the status of the play/pause button
+    /// </summary>
+    /// <param name="newStatus"></param>
+    public void setPlayPauseButtonStatus(ButtonState newStatus)
+    {
+        switch (newStatus)
+        {
+
+            case ButtonState.Paused:
+                PlayButton.GetComponentInChildren<UISprite>().spriteName = "play";
+                break;
+
+            case ButtonState.Playing:
+                PlayButton.GetComponentInChildren<UISprite>().spriteName = "pause";
+                break;
+
+        }
+    }
+
+    /// <summary>
+    /// changes the status of the record button
+    /// </summary>
+    /// <param name="newStatus"></param>
+    public void setRecordButtonStatus(ButtonState newStatus)
+    {
+        switch (newStatus)
+        {
+
+            case ButtonState.Recording:
+                RecordButton.GetComponentInChildren<UISprite>().spriteName = "record_pressed";
+                break;
+
+            case ButtonState.Not_Recording:
+                RecordButton.GetComponentInChildren<UISprite>().spriteName = "record_normal";
+                break;
+
+        }
+    }
+
+    /// <summary>
+    /// called when the play button is pressed
+    /// </summary>
+    public void playPauseButtonPressed()
+    {
+        //check the status and switch accordingly
+        switch (playButtonStatus)
+        {
+            case ButtonState.Paused:
+                start_playing();
+                break;
+
+            case ButtonState.Playing:
+                pause_playing();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// called when the play button is pressed
+    /// </summary>
+    public void recordButtonPressed()
+    {
+        //check the status and switch accordingly
+        switch (recordButtonStatus)
+        {
+            case ButtonState.Recording:
+                stop_recording();
+                break;
+
+            case ButtonState.Not_Recording:
+                start_recording();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// called when the stop button is pressed
+    /// </summary>
+    public void stopButtonPressed()
+    {
+        if (recordButtonStatus == ButtonState.Recording)
+        {
+            stop_recording();
+        }
+
+        else if (playButtonStatus == ButtonState.Playing)
+        {
+            stop_playing();
+        }
+
+        else setStatus("Nothing to stop, sorry!");
+    }
+
+    /// <summary>
+    /// stops playback
+    /// </summary>
+    public void stop_playing()
+    {
+        setStatus("Playback Stopped");
+        playButtonStatus = ButtonState.Playing;
+        setPlayPauseButtonStatus(ButtonState.Paused);
+    }
+
+    /// <summary>
+    /// starts playing
+    /// </summary>
+    public void pause_playing()
+    {
+        setStatus("Playback Paused");
+        playButtonStatus = ButtonState.Paused;
+        setPlayPauseButtonStatus(ButtonState.Paused);
+    }
+
+    /// <summary>
+    /// stops playing
+    /// </summary>
+    public void start_playing()
+    {
+        setStatus("Playing Recorded Movements");
+        playButtonStatus = ButtonState.Playing;
+        setPlayPauseButtonStatus(ButtonState.Playing);
+    }
+
+    /// <summary>
+    /// starts recording
+    /// </summary>
+    public void start_recording()
+    {
+        setStatus("Recording");
+        recordButtonStatus = ButtonState.Recording;
+        setRecordButtonStatus(ButtonState.Recording);
+    }
+
+    /// <summary>
+    /// stops recording
+    /// </summary>
+    public void stop_recording()
+    {
+        setStatus("Stopping Record");
+        recordButtonStatus = ButtonState.Not_Recording;
+        setRecordButtonStatus(ButtonState.Not_Recording);
+    }
+
+    /// <summary>
+    /// triggered when "new file" button pressed
+    /// </summary>
+    public void newButtonPressed()
+    {
+        setStatus("Created New File");
+    }
 
     /// <summary>
     /// triggered when save dialog selects a file to save to 
@@ -134,7 +349,7 @@ public class UIMenuActionReciever : MonoBehaviour {
     /// <summary>
     /// opens the file open dialog
     /// </summary>
-    public void openFile()
+    public void openFileButtonPressed()
     {
         setStatus("Opening File");
         LoadGroup.SetActiveRecursively(true);
@@ -143,7 +358,7 @@ public class UIMenuActionReciever : MonoBehaviour {
     /// <summary>
     /// saves the file save dialog
     /// </summary>
-    public void saveFile()
+    public void saveFileButtonPressed()
     {
         setStatus("Saving to File");
         SaveGroup.SetActiveRecursively(true);
@@ -152,7 +367,7 @@ public class UIMenuActionReciever : MonoBehaviour {
     /// <summary>
     /// exports the file export dialog
     /// </summary>
-    public void exportFile()
+    public void exportFileButtonPressed()
     {
         setStatus("Exporting to File");
         ExportGroup.SetActiveRecursively(true);
